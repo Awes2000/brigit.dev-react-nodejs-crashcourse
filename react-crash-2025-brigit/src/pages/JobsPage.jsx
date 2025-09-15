967af5fb18f990d79f1d58589c04944a50cfa76c
Awes <28003@ma-web.nl>
2025-09-15 08:56:31 +0800

    links, if else, state

---
+++ react-crash-2025-brigit/src/pages/JobsPage.jsx
@@ -0,0 +1,23 @@
+import data from "../jobs.json";
+import JobListing from "../components/JobListing";
+
+const JobsPage = () => {
+  const jobs = data.jobs;
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container mx-auto lg:container">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          All Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => (
+            <JobListing key={job.id} job={job} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobsPage;
